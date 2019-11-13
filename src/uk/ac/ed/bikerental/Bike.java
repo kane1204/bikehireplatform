753207b2bac8a5864ac46db5ab27be174424a018
Jonny K <31038591+kane1204@users.noreply.github.com>
2019-11-13 21:56:58 +0000

    Testing The commits on eclipse

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -2,7 +2,9 @@ package uk.ac.ed.bikerental;
 
 public class Bike {
     public BikeType getType() {
-        // TODO: Implement Bike.getType
+        // TODO: Implement Bike.getType hey bby 
+    	
+    	boolean jeff = true;
         assert false;
         return null;
     }
