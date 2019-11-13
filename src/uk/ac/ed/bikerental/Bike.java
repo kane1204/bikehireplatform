aba82b44b091be31b3a49ce6d145287e1cc89758
Jonny K <31038591+kane1204@users.noreply.github.com>
2019-11-13 22:00:03 +0000

    Reverted Changes

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -2,9 +2,7 @@ package uk.ac.ed.bikerental;
 
 public class Bike {
     public BikeType getType() {
-        // TODO: Implement Bike.getType hey bby 
-    	
-    	boolean jeff = true;
+        // TODO: Implement Bike.getType 
         assert false;
         return null;
     }
