b81cfb737d60c14fd8ebbdde86b7ee7e76720cf1
Jonny K <31038591+kane1204@users.noreply.github.com>
2019-11-19 19:36:37 +0000

    Quote stubs and variables

--- src/uk/ac/ed/bikerental/Quote.java
+++ src/uk/ac/ed/bikerental/Quote.java
@@ -1,5 +1,20 @@
 package uk.ac.ed.bikerental;
 
-public class Quote {
+import java.math.BigDecimal;
+import java.util.Collection;
 
+public class Quote {
+    public String providerName;
+    public BikeStore bikeStore;
+    public DateRange dates;
+    public BigDecimal totalPrice;
+    public BigDecimal totalDeposit;
+    public Collection<Bike> bikes;
+    
+    public BigDecimal calcTotalPrice(BigDecimal dailyPrice) {
+        return null;
+    }
+    public BigDecimal calcTotalDeposit(BigDecimal deposit) {
+        return null;
+    }
 }
